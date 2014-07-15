87498db27a9c714d5a367f6882f3836ba986d0a1
Joel Walters <jwalters@tableausoftware.com>
2014-07-14 18:03:44 -0700

    Fix things @iamEAP noted

--- test/src/Api/Assets/Email/DeploymentTest.php
+++ test/src/Api/Assets/Email/DeploymentTest.php
@@ -30,7 +30,7 @@ class DeploymentTest extends TestCase {
   /**
    * @test
    */
-  public function shouldSearchGroupsWithOptions() {
+  public function shouldSearchDeploymentsWithOptions() {
     $deployment_name = 'Foo Bar';
     $options = array('count' => 5);
     $expected_response = array('response');
@@ -83,6 +83,28 @@ class DeploymentTest extends TestCase {
     $this->assertEquals($expected_response, $api->show($deployment_id, $deployment_depth));
   }
 
+
+  /**
+   * @test
+   */
+  public function shouldShowDeploymentWithExtensions() {
+    $deployment_id = 7331;
+    $deployment_path = 'complete';
+    $deployment_extensions = 'extension123';
+    $expected_response = array('response');
+
+    $api = $this->getApiMock();
+    $api->expects($this->once())
+      ->method('get')
+      ->with('assets/email/deployment/' . $deployment_id, array(
+        'depth' => $deployment_path,
+        'extensions' => $deployment_extensions,
+      ))
+      ->will($this->returnValue($expected_response));
+
+    $this->assertEquals($expected_response, $api->show($deployment_id, $deployment_path, $deployment_extensions));
+  }
+
   /**
    * @test
    */
@@ -110,6 +132,31 @@ class DeploymentTest extends TestCase {
       ->will($this->returnValue($expected_response));
 
     $this->assertEquals($expected_response, $api->create($options));
+  }
+
+
+  /**
+   * @test
+   * @expectedException InvalidArgumentException
+   */
+  public function shouldThrowExceptionWhenCreatingDeploymentWithMissingParams()
+  {
+    $options = array(
+      'name' => 'Test Deployment',
+      // options excluded here for test
+      'email' => array(
+        'folderId' => 42,
+        'emailGroupId' => 420,
+        'subject' => 'Test Subject',
+      ),
+    );
+    $expected_response = array('response');
+
+    $api = $this->getApiMock();
+    $api->expects($this->any())
+      ->method('post');
+
+    $api->create($options);
   }
 
   protected function getApiClass() {
