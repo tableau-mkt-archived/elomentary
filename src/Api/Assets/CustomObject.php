f7b870eabe4adfc067127a3978a74c3a9011c4ad
Parris Varney <pvarney@npxconnect.com>
2014-07-25 09:33:05 -0400

    Fixed test: getInvalidCustomObjectCreateMeta() Reformatted per Drupal coding standards

--- src/Api/Assets/CustomObject.php
+++ src/Api/Assets/CustomObject.php
@@ -19,54 +19,54 @@ use Eloqua\Exception\InvalidArgumentException;
  * metadata.  To interface with the contents of a custom object, use
  * the Eloqua\Api\Data\CustomObject object
  */
-class CustomObject extends AbstractApi implements CreatableInterface, ReadableInterface, DestroyableInterface,  SearchableInterface {
+class CustomObject extends AbstractApi implements CreatableInterface, ReadableInterface, DestroyableInterface, SearchableInterface {
 
-    /**
-     * {@inheritdoc}
-     *
-     * CustomObject search searches by a custom object's name parameter
-     */
-    public function search($search, array $options = array()) {
-        return $this->get('assets/customObject', array_merge(array(
-            'search' => $search,
-        ), $options));
+  /**
+   * {@inheritdoc}
+   *
+   * CustomObject search searches by a custom object's name parameter
+   */
+  public function search($search, array $options = array()) {
+    return $this->get('assets/customObject', array_merge(array(
+      'search' => $search,
+    ), $options));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function show($id, $depth = 'complete', $extensions = NULL) {
+    return $this->get('assets/customObject/' . rawurlencode($id), array(
+      'depth' => $depth,
+    ));
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * @throws InvalidArgumentException
+   * @see http://topliners.eloqua.com/docs/DOC-3097
+   */
+  public function create($customObject_meta) {
+    if (!isset($customObject_meta['name'])) {
+      throw new InvalidArgumentException('At a minimum, you must provide an object name.');
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function show($id, $depth = 'complete', $extensions = null) {
-        return $this->get('assets/customObject/' . rawurlencode($id), array(
-            'depth' => $depth,
-        ));
-    }
-
-    /**
-     * {@inheritdoc}
-     *
-     * @throws InvalidArgumentException
-     * @see http://topliners.eloqua.com/docs/DOC-3097
-     */
-    public function create($customObject_meta) {
-        if (!isset($customObject_meta['name'])) {
-            throw new InvalidArgumentException('At a minimum, you must provide an object name.');
+    if (isset($customObject_meta['fields'])) {
+      foreach ($customObject_meta['fields'] as $field) {
+        if (!isset($field->dataType, $field->name)) {
+          throw new InvalidArgumentException('If defining fields, each must contained a dataType and name definition.');
         }
-
-        if (isset($customObject_meta['fields'])) {
-            foreach($customObject_meta['fields'] as $field) {
-                if (!isset($field->dataType, $field->name)) {
-                    throw new InvalidArgumentException('If defining fields, each must containd a dataType and name definition.');
-                }
-            }
-        }
-
-        return $this->post('assets/customObject', $customObject_meta);
+      }
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function remove($id) {
-        return $this->delete('assets/customObject/' . rawurlencode($id));
-    }
+    return $this->post('assets/customObject', $customObject_meta);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function remove($id) {
+    return $this->delete('assets/customObject/' . rawurlencode($id));
+  }
 }
--- test/src/Api/Assets/CustomObjectTest.php
+++ test/src/Api/Assets/CustomObjectTest.php
@@ -12,155 +12,159 @@ use Eloqua\Tests\Api\TestCase;
 
 class CustomObjectTest extends TestCase {
 
-    /**
-     * @test
-     */
-    public function shouldSearchCustomObjects() {
-        $custom_object_name = 'Test';
-        $expected_response = array('response');
+  /**
+   * @test
+   */
+  public function shouldSearchCustomObjects() {
+    $custom_object_name = 'Test';
+    $expected_response = array('response');
 
-        $api = $this->getApiMock();
-        $api->expects($this->once())
-            ->method('get')
-            ->with('assets/customObject', array('search' => $custom_object_name))
-            ->will($this->returnValue($expected_response));
+    $api = $this->getApiMock();
+    $api->expects($this->once())
+      ->method('get')
+      ->with('assets/customObject', array('search' => $custom_object_name))
+      ->will($this->returnValue($expected_response));
 
-        $this->assertEquals($expected_response, $api->search($custom_object_name));
-    }
+    $this->assertEquals($expected_response, $api->search($custom_object_name));
+  }
 
-    /**
-     * @test
-     */
-    public function shouldSearchCustomObjectsWithOptions() {
-        $custom_object_name = 'Test';
-        $options = array('count' => 5);
-        $expected_response = array('response');
+  /**
+   * @test
+   */
+  public function shouldSearchCustomObjectsWithOptions() {
+    $custom_object_name = 'Test';
+    $options = array('count' => 5);
+    $expected_response = array('response');
 
-        $api = $this->getApiMock();
-        $api->expects($this->once())
-            ->method('get')
-            ->with('assets/customObject', array_merge(array('search' => $custom_object_name), $options))
-            ->will($this->returnValue($expected_response));
+    $api = $this->getApiMock();
+    $api->expects($this->once())
+      ->method('get')
+      ->with('assets/customObject', array_merge(array('search' => $custom_object_name), $options))
+      ->will($this->returnValue($expected_response));
 
-        $this->assertEquals($expected_response, $api->search($custom_object_name, $options));
-    }
+    $this->assertEquals($expected_response, $api->search($custom_object_name, $options));
+  }
 
-    /**
-     * @test
-     */
-    public function shouldShowCustomObjectsJustId() {
-        $custom_object_id = 1337;
-        $expected_response = array('response');
+  /**
+   * @test
+   */
+  public function shouldShowCustomObjectsJustId() {
+    $custom_object_id = 1337;
+    $expected_response = array('response');
 
-        $api = $this->getApiMock();
-        $api->expects($this->once())
-            ->method('get')
-            ->with('assets/customObject/' . $custom_object_id, array(
-                'depth' => 'complete',
-            ))
-            ->will($this->returnValue($expected_response));
+    $api = $this->getApiMock();
+    $api->expects($this->once())
+      ->method('get')
+      ->with('assets/customObject/' . $custom_object_id, array(
+        'depth' => 'complete',
+      ))
+      ->will($this->returnValue($expected_response));
 
-        $this->assertEquals($expected_response, $api->show($custom_object_id));
-    }
+    $this->assertEquals($expected_response, $api->show($custom_object_id));
+  }
 
-    public function shouldShowCustomObjectsWithDepth() {
-        $custom_object_id = 1337;
-        $depth = 'minimal';
-        $expected_response = array('response');
+  public function shouldShowCustomObjectsWithDepth() {
+    $custom_object_id = 1337;
+    $depth = 'minimal';
+    $expected_response = array('response');
 
-        $api = $this->getApiMock();
-        $api->expects($this->once())
-            ->method('get')
-            ->with('assets/customObject/' . $custom_object_id, array(
-                'depth' => $depth,
-            ))
-            ->will($this->returnValue($expected_response));
+    $api = $this->getApiMock();
+    $api->expects($this->once())
+      ->method('get')
+      ->with('assets/customObject/' . $custom_object_id, array(
+        'depth' => $depth,
+      ))
+      ->will($this->returnValue($expected_response));
 
-        $this->assertEquals($expected_response, $api->show($custom_object_id, $depth));
-    }
+    $this->assertEquals($expected_response, $api->show($custom_object_id, $depth));
+  }
 
-    /**
-     * @test
-     */
-    public function shouldRemoveCustomObjects() {
-        $custom_object_id = 1337;
+  /**
+   * @test
+   */
+  public function shouldRemoveCustomObjects() {
+    $custom_object_id = 1337;
 
-        $api = $this->getApiMock();
-        $api->expects($this->once())
-            ->method('delete')
-            ->with('assets/customObject/' . $custom_object_id);
+    $api = $this->getApiMock();
+    $api->expects($this->once())
+      ->method('delete')
+      ->with('assets/customObject/' . $custom_object_id);
 
-        $this->assertNull($api->remove($custom_object_id));
-    }
+    $this->assertNull($api->remove($custom_object_id));
+  }
 
-    /**
-     * @test
-     * @dataProvider getValidCustomObjectCreateMeta
-     */
-    public function shouldCreateCustomObject($customObject_meta) {
-        $expected_response = array('response');
+  /**
+   * @test
+   * @dataProvider getValidCustomObjectCreateMeta
+   */
+  public function shouldCreateCustomObject($customObject_meta) {
+    $expected_response = array('response');
 
-        $api = $this->getApiMock();
-        $api->expects($this->once())
-            ->method('post')
-            ->with('assets/customObject', $customObject_meta)
-            ->will($this->returnValue($expected_response));
+    $api = $this->getApiMock();
+    $api->expects($this->once())
+      ->method('post')
+      ->with('assets/customObject', $customObject_meta)
+      ->will($this->returnValue($expected_response));
 
-        $this->assertEquals($expected_response, $api->create($customObject_meta));
-    }
+    $this->assertEquals($expected_response, $api->create($customObject_meta));
+  }
 
-    public function getValidCustomObjectCreateMeta() {
-        return array (
-            array (
-                array (
-                    'name' => 'Test object with defined fields',
-                    'fields' => array (
-                        (object) array (
-                            'name' => 'Field one',
-                            'dataType' => 'text',
-                        ),
-                    ),
-                ),
+  public function getValidCustomObjectCreateMeta() {
+    return array(
+      array(
+        array(
+          'name' => 'Test object with defined fields',
+          'fields' => array(
+            (object) array(
+              'name' => 'Field one',
+              'dataType' => 'text',
             ),
-            array (
-                array (
-                    'name' => 'Test object without defined fields',
-                ),
-            ),
-        );
-    }
+          ),
+        ),
+      ),
+      array(
+        array(
+          'name' => 'Test object without defined fields',
+        ),
+      ),
+    );
+  }
 
-    /**
-     * @test
-     * @expectedException InvalidArgumentException
-     * @dataProvider getInvalidCustomObjectCreateMeta
-     */
-    public function shouldThrowExceptionCreateCustomObject($customObject_meta) {
-        $api = $this->getApiMock();
-        $api->create($customObject_meta);
-    }
+  /**
+   * @test
+   * @expectedException InvalidArgumentException
+   * @dataProvider getInvalidCustomObjectCreateMeta
+   */
+  public function shouldThrowExceptionCreateCustomObject($customObject_meta) {
+    $api = $this->getApiMock();
+    $api->create($customObject_meta);
+  }
 
-    public function getInvalidCustomObjectCreateMeta() {
-        return array (
-            array (
-                'description' => 'Test object missing name definition',
+  public function getInvalidCustomObjectCreateMeta() {
+    return array(
+      array(
+        array(
+          'description' => 'Test object missing name definition',
+        ),
+      ),
+      array(
+        array(
+          'name' => 'Invalid fields definition ',
+          'fields' => array(
+            (object) array(
+              'name' => 'Correctly defined field',
+              'dataType' => 'text',
             ),
-            array (
-                'name' => 'Invalid fields definition ',
-                'fields' => array (
-                    (object) array (
-                        'name' => 'Correctly defined field',
-                        'dataType' => 'text',
-                    ),
-                    (object) array (
-                        'name' => 'Missing dataType field',
-                    ),
-                ),
+            (object) array(
+              'name' => 'Missing dataType field',
             ),
-        );
-    }
+          ),
+        ),
+      ),
+    );
+  }
 
-    protected function getApiClass() {
-        return 'Eloqua\Api\Assets\CustomObject';
-    }
+  protected function getApiClass() {
+    return 'Eloqua\Api\Assets\CustomObject';
+  }
 }
