b7cf97b1c58ebc56f1dd2af76adaafad119c9ae6
Parris Varney <pvarney@npxconnect.com>
2014-08-07 09:30:49 -0400

    Forgot to stage a file in the last commit

--- test/src/ClientTest.php
+++ test/src/ClientTest.php
@@ -166,6 +166,10 @@ class ClientTest extends \PHPUnit_Framework_TestCase {
       array('emails', 'Eloqua\Api\Assets\Email'),
       array('customObject', 'Eloqua\Api\Assets\CustomObject'),
       array('customObjects', 'Eloqua\Api\Assets\CustomObject'),
+      array('optionList', 'Eloqua\Api\Assets\OptionList'),
+      array('optionLists', 'Eloqua\Api\Assets\OptionList'),
+      array('visitor', 'Eloqua\Api\Data\Visitor'),
+      array('visitors', 'Eloqua\Api\Data\Visitor'),
     );
   }
 
@@ -176,19 +180,6 @@ class ClientTest extends \PHPUnit_Framework_TestCase {
   public function shouldNotGetApiInstance() {
     $client = new Client();
     $client->api('not_a_thing');
-  }
-
-  public function getApiClassProvider() {
-    return array(
-      array('contact', 'Eloqua\Api\Data\Contact'),
-      array('contacts', 'Eloqua\Api\Data\Contact'),
-      array('email', 'Eloqua\Api\Assets\Email'),
-      array('emails', 'Eloqua\Api\Assets\Email'),
-      array('optionList', 'Eloqua\Api\Assets\OptionList'),
-      array('optionLists', 'Eloqua\Api\Assets\OptionList'),
-      array('visitor', 'Eloqua\Api\Data\Visitor'),
-      array('visitors', 'Eloqua\Api\Data\Visitor'),
-    );
   }
 
   public function getHttpClientMock(array $methods = array()) {
