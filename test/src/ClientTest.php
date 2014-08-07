7e6d110a5a915a5374bb28f96e10370f7ed4d819
Parris Varney <pvarney@npxconnect.com>
2014-08-07 13:35:42 -0400

    Remove issue from merge conflict

--- test/src/ClientTest.php
+++ test/src/ClientTest.php
@@ -178,13 +178,6 @@ class ClientTest extends \PHPUnit_Framework_TestCase {
     $client->api('not_a_thing');
   }
 
-  public function getApiClassProvider() {
-    return array(
-      array('contact', 'Eloqua\Api\Data\Contact'),
-      array('contacts', 'Eloqua\Api\Data\Contact'),
-    );
-  }
-
   public function getHttpClientMock(array $methods = array()) {
     $methods = array_merge(
       array('get', 'post', 'patch', 'put', 'delete', 'request', 'setOption', 'setHeaders', 'authenticate'),
