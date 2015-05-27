69819fff70358c0f5e9353063dddcd4533505859
Eric Peterson <epeterson@tableausoftware.com>
2015-05-26 17:58:40 -0700

    Adding full test coverage for custom object data.

--- test/src/Api/Data/CustomObjectTest.php
+++ test/src/Api/Data/CustomObjectTest.php
@@ -43,6 +43,28 @@ class CustomObjectTest extends TestCase {
     $api->create($data);
   }
 
+  /**
+   * @test
+   */
+  public function shouldShowCustomObjectData() {
+    $expected = 'test';
+    $customObjectId = 1;
+    $expectedShowId = 123;
+
+    $api = $this->getApiMock(array($customObjectId));
+    $api->expects($this->once())
+      ->method('get')
+      ->with("data/customObject/$customObjectId", array(
+        'search' => 'id=' . $expectedShowId,
+        'depth' => 'complete',
+      ))
+      ->will($this->returnValue($expected));
+
+    $result = $api->show($expectedShowId);
+
+    $this->assertEquals($expected, $result);
+  }
+
   protected function getApiClass() {
     return 'Eloqua\Api\Data\CustomObject';
   }
